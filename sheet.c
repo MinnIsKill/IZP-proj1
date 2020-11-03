6dc5e5524c520f1f581cfad1354156f9f7625967
MinnIsKill <72144428+MinnIsKill@users.noreply.github.com>
2020-11-03 13:37:47 +0100

    (meaningless) update

--- sheet.c
+++ sheet.c
@@ -19,7 +19,7 @@ ignorovan. Prvni znak z retezce DELIM bude take pouzit jako oddelovac vystupnich
 
 Prikazy pro upravu tabulky:
 
-///irow R - vlozi radek tabulky pred radek R > 0 (insert-row).
+irow R - vlozi radek tabulky pred radek R > 0 (insert-row).
 arow - prida novy radek tabulky na konec tabulky (append-row).
 drow R - odstrani radek cislo R > 0 (delete-row).
 drows N M - odstrani radky N až M (N <= M). V případě N=M se příkaz chová stejně jako drow N.
@@ -52,7 +52,7 @@ cmin C N M - obdobne jako csum, avsak vysledna hodnota predstavuje nejmensi nale
 cmax C N M - obdobne jako cmin, jedna se vsak o maximalni nalezenou hodnotu.
 ccount C N M - obdobne jako csum, avsak vysledna hodnota predstavuje pocet neprazdnych hodnot danych bunek.
 cseq N M B - do bunek ve sloupcich N az M vcetne vlozi postupne rostouci cisla (o jednicku) pocinaje hodnotou B.
-rseq C N M B - ve sloupci C do buněk každého řádku od řádku N po řádek M včetně vloží rostoucí cisla pocinaje hodnotou B. Cislo M muze byt nahrazeno pomlckou. V takovem pripade se tim mysli posledni radek souboru.
+rseq C N M B - ve sloupci C do bunek kazdeho radku od radku N po radek M vcetne vlozi rostouci cisla pocinaje hodnotou B. Cislo M může byt nahrazeno pomlckou. V takovem pripade se tim mysli posledni radek souboru.
 rsum C N M - do bunky ve sloupci C na radku M+1 vlozi soucet hodnot bunek ve sloupci C na radcich N az M vcetne.
 ravg C N M - obdobne jako rsum, avsak vysledna hodnota predstavuje aritmeticky prumer.
 rmin C N M - obdobne jako rsum, avsak vysledna hodnota predstavuje nejmensi hodnotu.
@@ -61,8 +61,11 @@ rcount C N M - obdobne jako rsum, avsak vysledna hodnota predstavuje pocet nepra
 
 **/
 
-int main()
+/// VSCode Keybind-sheet:  CTRL+SHIFT+B -> BUILD  |  F5 -> DEBUG
+
+int main(int argc, char *argv[])
 {
+    
     printf("Neco tu jako musi prozatim byt no...\n");
     return 0;
 }
